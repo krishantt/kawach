4e6f99abe80e03c7116a4e41019db40e1785d25c
bigya01 <106421905+bigya01@users.noreply.github.com>
2023-12-23 11:30:08 +0545

    solidity

---
+++ truffle/contracts/documents.sol
@@ -0,0 +1,53 @@
+//program coding
+//// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12 <0.9.0;
+
+
+contract DocumentStorage {
+    // Mapping to store whitelisted addresses
+    mapping(address => bool) public whitelist;
+
+    // Mapping to store document hashes and metadata
+    mapping(bytes32 => Document) public documents;
+
+    // Struct to represent document metadata
+    struct Document {
+        string name;
+        string owner;
+    }
+
+    // Modifier to restrict access to whitelisted users
+    modifier onlyWhitelisted() {
+        require(whitelist[msg.sender], "Caller is not whitelisted");
+        _;
+    }
+
+    // Constructor whitelists two specific users
+    constructor(address user1, address user2) {
+         whitelist[user1] = true;
+        whitelist[user2] = true;
+    }
+
+    // Function to upload a new document (restricted to whitelisted users)
+    function uploadDocument(string memory _name, string memory _documentHash) public onlyWhitelisted {
+        // Validate input
+      
+
+        // Generate unique document identifier
+        bytes32 documentId = keccak256(abi.encodePacked(_name, _documentHash, msg.sender));
+
+        // Ensure document doesn't already exist
+
+        // Store document metadata
+        // documents[documentId] = Document({
+        //     name: _name,
+        //     owner
+        // });
+
+        // Emit event for external tracking
+        emit DocumentUploaded(documentId, _name, _documentHash, msg.sender);
+    }
+
+    // Event for tracking document uploads
+    event DocumentUploaded(bytes32 indexed documentId, string name, string documentHash, address indexed uploader);
+}
---
+++ truffle/contracts/info.sol
@@ -0,0 +1,30 @@
+//program coding
+//// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12 <0.9.0;
+
+contract info{
+    mapping(address => bool) public whitelist;
+        mapping(bytes32 => individual_info) public documents;
+
+    struct individual_info{
+        string name;
+        address contact_no;
+        bool verified_by_gov;
+        address []document;
+
+    }
+    modifier onlyWhitelisted() {
+        require(whitelist[msg.sender], "you dont have access to this role");
+        _;
+    }
+    //adding address of user and government
+    // constructor(address user, address government) {
+    //     whitelist[user] = true;
+    //     whitelist[government] = true;
+    // }
+    //tasks only government and user can accomplish
+    function uploadData(address new_document)public onlyWhitelisted{
+        //
+    }
+    
+}
